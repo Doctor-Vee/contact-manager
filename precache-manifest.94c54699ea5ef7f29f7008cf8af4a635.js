7c5973106abbc5f7c90d7f5ebb823e7f5064e43f
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-08-20 14:12:37 +0100

    Updates

---
+++ precache-manifest.94c54699ea5ef7f29f7008cf8af4a635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c0999f33ac3772fc14dfaf6f701058",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "2bdc13f76f838b78c46a",
+    "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "222f1793cbdf8b2e3fa6",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2bdc13f76f838b78c46a",
+    "url": "/contact-manager/static/js/2.5b5b5e0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.5b5b5e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222f1793cbdf8b2e3fa6",
+    "url": "/contact-manager/static/js/main.a9563215.chunk.js"
+  },
+  {
+    "revision": "8dc9346f41ed168d3261",
+    "url": "/contact-manager/static/js/runtime-main.2e7bbdb6.js"
+  }
+]);
