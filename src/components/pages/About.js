20f628d9ae382e8b23e3a40ce08c299dc5932c6f
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-10-05 13:27:46 +0100

    modify about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div>
             <h1 className="display-4">About Contact Manager</h1>
-            <p className="lead">Simple app for managing contacts</p>
+            <p className="lead">Simple app for managing contacts by Doctor Vee using Brad Traversy's tutorial</p>
             <p className="text-secondary">Version 1.0.0</p>
         </div>
     )
