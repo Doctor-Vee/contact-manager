cc867b3f61ebdad436ec0d5f2cce030995b9f7c4
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-10-05 13:29:48 +0100

    modify NotFound component

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -5,7 +5,7 @@ export default function NotFound() {
         <div>
             <h1 className="display-2 text-danger">404</h1>
             <h2 className="display-4">Page Not Found</h2>
-            <p className="lead">Sorry, that page does not exist</p>
+            <p className="lead">Sorry, the page does not exist</p>
         </div>
     )
 }
